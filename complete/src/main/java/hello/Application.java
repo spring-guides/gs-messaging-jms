35c80a93b07d1513b5831e03bbfea7d102b9c257
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 13:20:07 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,25 +2,22 @@
 package hello;
 
 import java.io.File;
-
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.jms.listener.SimpleMessageListenerContainer;
 import org.springframework.jms.listener.adapter.MessageListenerAdapter;
 import org.springframework.util.FileSystemUtils;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     static String mailboxDestination = "mailbox-destination";
