16e3da0d6499208f0a9a8487438735282b65c337
Jay Bryant <jbryant@vmware.com>
2022-12-07 11:35:21 -0600

    Upgrade to Spring Boot 3.0.0
    to get onto the latest major version of Spring Boot.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,7 +1,7 @@
 
 package hello;
 
-import javax.jms.ConnectionFactory;
+import jakarta.jms.ConnectionFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
