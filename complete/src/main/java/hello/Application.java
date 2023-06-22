f5fd6ac0d58376d7d1cfb785dd4b2f090ae1514a
Stephane Nicoll <snicoll@vmware.com>
2023-06-22 11:54:51 +0200

    Update guide to use Artemis rather than ActiveMQ
    Closes gh-39

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -24,9 +24,9 @@ public class Application {
 	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
 													DefaultJmsListenerContainerFactoryConfigurer configurer) {
 		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-		// This provides all boot's default to this factory, including the message converter
+		// This provides all auto-configured defaults to this factory, including the message converter
 		configurer.configure(factory, connectionFactory);
-		// You could still override some of Boot's default if necessary.
+		// You could still override some of settings if necessary.
 		return factory;
 	}
 
